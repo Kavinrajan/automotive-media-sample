f8981c7758a13380d271d166913d1e59e4085778
kavinrajan <kavinece53@gmail.com>
2023-12-08 00:43:27 +0530

    automotive media initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "automotive-media-sample"
+include(":mobile")
+include(":automotive")
+include(":shared")
+ 
